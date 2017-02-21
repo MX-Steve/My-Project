145edaaedfb347e66a355a83ec6aae233b371dd2
MX-Steve <2513556525@qq.com>
2017-02-21 20:44:28 +0800

    bom-02

---
+++ execise/day05 素材/js/6.js
@@ -0,0 +1,15 @@
++function(){
+	var pop=document.getElementById("pop");
+	pop.addEventListener("click",function(){
+		alert("约吗");
+	});
+	//获得pop大小
+	//最大距离
+	var MAXLEFT=innerWidth-200;
+	var MAXTOP=innerHeight-200;
+	//绑定mouseover
+	
+	//如果新位置不包含鼠标位置
+		//设置到新位置
+
+}();
