825843a7a306a6462733697013ac4381f8a4eb73
MX-Steve <2513556525@qq.com>
2017-02-23 10:29:00 +0800

    game change

--- tetris 素材/js/Shape.js
+++ tetris 素材/js/Shape.js
@@ -1,57 +1,83 @@
-/*
-	专门描述格子和图片的数据结构和功能
-*/
-//定义Cell类型描述一个格子
+/**
+ * 专门描述格子和图形的数据结构和功能
+ */
+//定义Cell类型描述一个格子: r,c,src
 function Cell(r,c,src){
 	this.r=r; this.c=c; this.src=src;
 }
-/*
-function Cell(r,c,src){
-	this.r=r;this.c=c;this.src=src;
-}
-/
-/*定义所有图形的的公共父类型Shape
-		所有图形都有一个格子数组，包含四个格子
-		所有图片否可以下落左移，右移，旋转
- */
- function Shape(r0,c0,r1,c1,r2,c2,r3,c3,src,states,orgi){
+/*定义所有图形的公共父类型Shape:
+	    所有图形都有一个格子数组，包含四个格子
+      所有图形都可下落，左移，右移，旋转...
+*/
+function Shape(r0,c0,r1,c1,r2,c2,r3,c3,src,states,orgi){
 	this.cells=[
-		new Cell(r0,c0,src),
-		new Cell(r1,c1,src),
-		new Cell(r2,c2,src),
-		new Cell(r3,c3,src),
+		new Cell(r0,c0,src),	 new Cell(r1,c1,src),	
+		new Cell(r2,c2,src),	 new Cell(r3,c3,src),	
 	];
-	this.states=states;//保存状态
-	this.orgCell=this.cell[orgi];//根据下标获得参照格对象
-	this.statei=0;
- }
+	this.states=states;//保存图形的多个旋转状态
+	//根据参照格的下标，获得参照格对象
+	this.orgCell=this.cells[orgi];
+	this.statei=0;//保存图形所处的当前状态序号
+}
 Shape.prototype={//重定义Shape的原型对象
-	moveDown(){//图片下落
+	moveDown(){//图形下落: 
 		//遍历当前图形的cells中每个cell
 		for(var i=0;i<this.cells.length;i++)
-			this.cells[i].r++;
+			this.cells[i].r++;//当前cell的r+1
 	},
-	moveLeft(){
+	moveLeft(){//图形左移
+		//遍历当前图形的cells中每个cell
 		for(var i=0;i<this.cells.length;i++)
-			this.cells[i].c--;
+			this.cells[i].c--;	//当前cell的c-1
 	},
-	moveRight(){
+	moveRight(){//图形右移
+		//遍历当前图形的cells中每个cell
 		for(var i=0;i<this.cells.length;i++)
-		this.cells[i].c++;
+			this.cells[i].c++;	//当前cell的c+1
 	},
-	rotateR(){//顺时针
-		
+	rotateR(){//顺时针旋转
+		this.statei++;//将statei+1
+		//如果statei等于states的元素个数,就改回0
+		if(this.statei==this.states.length)
+			this.statei=0;
+		this.rotate();//旋转
 	},
-	rotateL(){//逆时针
-	
+	rotate(){
+		//获得state中statei位置的状态对象
+		var state=this.states[this.statei];
+		//遍历当前图形的cells中每个cell
+		for(var i=0;i<this.cells.length;i++){
+			var cell=this.cells[i];//临时保存cell
+			//如果cell不是orgCell
+			if(cell!=this.orgCell){
+				//       r0c0 r1c1  r2c2 r3c3
+				//state:{-1,0, 0,0, +1,0, 0,-1}
+				//i=0
+				//用orgCell的r加上state中对应位置的值，计算出cell的新r
+				cell.r=this.orgCell.r+state["r"+i];
+				//用orgCell的c加上state中对应位置的值，计算出cell的新c
+				cell.c=this.orgCell.c+state["c"+i];
+			}
+		}
 	},
+	rotateL(){//逆时针旋转
+		this.statei--;//将statei-1
+		//如果statei等于-1,就改回states的元素个数-1
+		if(this.statei==-1) 
+			this.statei=this.states.length-1;
+		this.rotate();//旋转
+	}
 }
-//旋转状态
-/*
-	为每种图形定义专门的类型
-		子类型的构造函数中借用shape构造函数，传入每个
-			格子的坐标和图片路径
-		子类型的原型要继承shape的原型
+//定义类型State,描述一种旋转状态: 
+function State(r0,c0,r1,c1,r2,c2,r3,c3){
+	this.r0=r0; this.c0=c0;
+	this.r1=r1; this.c1=c1;
+	this.r2=r2; this.c2=c2;
+	this.r3=r3; this.c3=c3;
+}
+/*为每种图形定义专门的类型:
+    子类型的构造函数中借用Shape构造函数，传入每个格子的坐标和图片路径
+		子类型的原型要继承Shape的原型
 */
 function T(){
 	Shape.call(this,
@@ -60,36 +86,29 @@ function T(){
 		[
 			new State(0,-1, 0,0, 0,+1, +1,0),
 			new State(-1,0, 0,0, +1,0, 0,-1),
-			new State(0,+1, 0,0, 0,-1, -1,0),
-			new State(+1,0, 0,0, -1,0, 0,+1)
+		  new State(0,+1, 0,0, 0,-1, -1,0),
+		  new State(+1,0, 0,0, -1,0, 0,+1)
 		],
 		1)
 }
 Object.setPrototypeOf(T.prototype,Shape.prototype);
 function I(){
-	Shape.call(this,0,3,0,4,0,5,0,6,"img/I.png",
+	Shape.call(this,
+		0,3,0,4,0,5,0,6,
+		"img/I.png",
 		[
 			new State(0,-1, 0,0, 0,+1, 0,+2),
-			new State(-1,0, 0,0, +1,0, +2,0)
-		]
-		,1)
+			new State(-1,0, 0,0, +1,0, +2,0),
+		],
+		1)
 }
 Object.setPrototypeOf(I.prototype,Shape.prototype);
 function O(){
-	Shape.call(this,0,4,0,5,1,4,1,5,"img/O.png",
+	Shape.call(this,
+		0,4,0,5,1,4,1,5,
+		"img/O.png",
 		[new State(0,-1, 0,0, +1,-1, +1,0)],
 		1)
 }
 Object.setPrototypeOf(O.prototype,Shape.prototype);
-function J(){
-	Shape.call(this,0,5,1,5,2,5,2,4,"img/J.png")
-}
-Object.setPrototypeOf(J.prototype,Shape.prototype);
-function L(){
-	Shape.call(this,0,5,1,5,2,5,2,6,"img/L.png")
-}
-Object.setPrototypeOf(L.prototype,Shape.prototype);
-function Z(){
-	Shape.call(this,0,4,0,5,1,5,1,6,"img/J.png")
-}
-Object.setPrototypeOf(Z.prototype,Shape.prototype);
+console.dir(new T());
--- tetris 素材/js/tetris.js
+++ tetris 素材/js/tetris.js
@@ -1,35 +1,148 @@
 var game={
-	RN:20,CN:10,//总行、列
-	CSIZE:26,//每个格子大小
-	OFFSET:15,//内边距
+	RN:20,CN:10,//总行数,列数
+	CSIZE:26,OFFSET:15,//每个格子大小和游戏容器内边距
+	//保存游戏容器
 	pg:document.querySelector(".playground"),
-	shape:null,//保存主角图形
-	timer:null,interval:200,
-	wall:null,//方块墙
+	shape:null,nextShape:null,//保存主角图形
+	//保存下落速度和定时器序号
+	interval:200,timer:null,
+	lines:0,score:0,
+		SCORES:[0,10,30,60,100],//得分数组
+	wall:null,//1. 方块墙
 	start(){//启动游戏
-		this.wall=[];
+		this.lines=this.score=0;
+		this.wall=[];//2. 创建空数组保存在wall中
+		//r从0开始，到<RN结束
 		for(var r=0;r<this.RN;r++){
+			//向wall中添加一个新的CN个空元素的数组
 			this.wall[r]=new Array(this.CN);
 		}
-		//创建一个图形，保存在shape中
-		this.shape=new O();
-		this.paintShape();//绘制主角
+		this.shape=this.randomShape();//创建新图形，保存在shape中
+		this.nextShape=this.randomShape();
 		this.paint();//重绘一切
-		this.timer=setInterval(	
+		//启动周期性定时器,调用moveDown
+		this.timer=setInterval(
 			this.moveDown.bind(this),this.interval);
 		//为网页绑定键盘按下事件
 		document.onkeydown=function(e){
-			switch(e.keyCode){
+			switch(e.keyCode){//判断按键号
 				case 40: this.moveDown(); break;
 				case 37: this.moveLeft(); break;
 				case 39: this.moveRight(); break;
 				case 32: this.hardDrop(); break;
+				case 38: this.rotateR(); break;
+				case 90: this.rotateL(); break;
 			}
 		}.bind(this);
 	},
+	randomShape(){
+		switch(Math.floor(Math.random()*3)){
+			case 0: return new O();
+			case 1: return new I();
+			case 2: return new T();
+		}
+	},
+	canRotate(){
+		for(var i=0;i<this.shape.cells.length;i++){
+			var cell=this.shape.cells[i];
+			if(cell.r<0||cell.r>=this.RN||cell.c<0||cell.c>=this.CN
+				||this.wall[cell.r][cell.c]){
+				return false;
+			}
+			return true;
+		}
+	},
+	rotateR(){
+		this.shape.rotateR();
+		if(this.canRotate())
+			this.paint();
+		else
+			this.shape.rotateL();
+	},
+	rotateL(){
+		this.shape.rotateL();
+		if(this.canRotate())
+			this.paint();
+		else 
+			this.shape.rotateR();
+	},
 	hardDrop(){
-		while(this.canDown())
+		while(this.canDown()){
 			this.moveDown();
+		}
+	},
+	canLeft(){//判断能否左移
+		//遍历shape中每个cell
+		for(var i=0;i<this.shape.cells.length;i++){
+			var cell=this.shape.cells[i];//临时保存cell
+			if(cell.c==0)//如果cell的c等于0
+				return false;//就返回false
+			//否则，如果wall中cell左侧有格
+			else if(this.wall[cell.r][cell.c-1])
+				return false;//就返回false
+		}
+		return true;//就返回true
+	},
+	moveLeft(){
+		if(this.canLeft())//如果可以左移
+			this.shape.moveLeft();
+	},
+	canRight(){//判断能否右移
+		//遍历shape中每个cell
+		for(var i=0;i<this.shape.cells.length;i++){
+			var cell=this.shape.cells[i];//临时保存cell
+			if(cell.c==this.CN-1)//如果cell的c等于CN-1
+				return false;//就返回false
+			//否则，如果wall中cell右侧有格
+			else if(this.wall[cell.r][cell.c+1])
+				return false;//就返回false
+		}
+		return true;
+	},
+	moveRight(){
+		if(this.canRight())//如果可以右移
+			this.shape.moveRight();
+	},
+	paintShape(){//绘制主角图形
+		//创建文档片段frag
+		var frag=document.createDocumentFragment();
+		//遍历shape的cells中每个cell
+		for(var i=0;i<this.shape.cells.length;i++){
+			//将当前cell临时保存在cell中
+			var cell=this.shape.cells[i];
+			var img=new Image();//新建一个img
+			//设置img的left为cell的c*CSIZE+OFFSET
+			img.style.left=
+				cell.c*this.CSIZE+this.OFFSET+"px";
+			//设置img的top为cell的r*CSIZE+OFFSET
+			img.style.top=
+				cell.r*this.CSIZE+this.OFFSET+"px";
+			img.src=cell.src;//设置img的src为cell的src
+			frag.appendChild(img);//将img追加到frag中
+		}
+		this.pg.appendChild(frag);//将frag追加到pg
+	},
+	paintNext(){
+		var frag=document.createDocumentFragment();
+		for(var i=0;i<this.nextShape.cells.length;i++){
+			var cell=this.nextShape.cells[i];
+			var img=new Image();
+			img.style.left=(cell.c+10)*this.CSIZE+this.OFFSET+"px";
+			img.style.top=(cell.r+1)*this.CSIZE+this.OFFSET+"px";
+			img.src=cell.src;
+			frag.appendChild(img);
+		}
+		this.pg.appendChild(frag);
+	},
+	paint(){//重绘一切:包含一切重绘方法
+		//先删除pg中所有旧的方块img
+		var reg=/<img [^>]*>/g;
+		this.pg.innerHTML=
+			this.pg.innerHTML.replace(reg,"");
+		this.paintShape();//重绘主角
+		this.paintWall();//7. 重绘方块墙
+		this.paintNext();
+		this.paintScore();
 	},
 	canDown(){//判断能否下落
 		//遍历shape的cells中每个cell
@@ -38,47 +151,70 @@ var game={
 			if(cell.r==this.RN-1)//如果cell的r等于RN-1
 				return false;//就返回false
 			else if(this.wall[cell.r+1][cell.c])
-				//5. 否则如果wall中cell下方有格,就返回false
-				return false;//就返回false
+			//5. 否则如果wall中cell下方有格,就返回false
+				return false;
 		}
-		return true;
+		return true;//就返回true
+	},
+	landIntoWall(){//4. 将shape中每个cell落入墙中
+		//遍历shape的cells中每个cell
+		for(var i=0;i<this.shape.cells.length;i++){
+			//将当前cell临时保存
+			var cell=this.shape.cells[i];
+			//将当前cell保存到wall中相同位置
+			this.wall[cell.r][cell.c]=cell;
+		}
+	},
+	paintWall(){//6. 专门绘制方块墙
+		//创建文档片段frag
+		var frag=document.createDocumentFragment();
+		//遍历wall中每个元素
+		for(var r=this.RN-1;r>=0;r--){
+			//如果当前行不是空行
+			if(this.wall[r].join("")!=""){
+				for(var c=0;c<this.CN;c++){
+					//如果wall中当前位置有格
+					if(this.wall[r][c]){
+						//将当前cell临时保存在cell中
+						var cell=this.wall[r][c];
+						var img=new Image();//新建一个img
+						//设置img的left为cell的c*CSIZE+OFFSET
+						img.style.left=
+							cell.c*this.CSIZE+this.OFFSET+"px";
+						//设置img的top为cell的r*CSIZE+OFFSET
+						img.style.top=
+							cell.r*this.CSIZE+this.OFFSET+"px";
+						//设置img的src为cell的src
+						img.src=cell.src;
+						frag.appendChild(img);//将img追加到frag中
+					}
+				}
+			}else break;//否则说明上方不再有格，就退出
+		}
+		this.pg.appendChild(frag);//将frag追加到pg中
 	},
 	moveDown(){//让主角图形下落一步
-		if(this.canDown()){
-			this.shape.moveDown();
-		}else{
-			this.landIntoWall();//shape中的cell放入wall中
-			this.deleteRows();//判断并删除满格行
-			this.shape=new O();//生成新图形
+		if(this.canDown()){//如果可以下落
+			this.shape.moveDown();//才让主角下落
+		}else{//停止下落
+			this.landIntoWall();//3.shape的cell放入wall中
+			var ln=this.deleteRows();//判断并删除满格行
+			this.lines+=ln;
+			this.score+=this.SCORES[ln];
+			this.shape=this.nextShape;
+			this.nextShape=this.randomShape();//生成新图形
 		}
 		this.paint();//重绘一切
 	},
-	canLeft(){
-		for(var i=0;i<this.shape.cells.length;i++){
-			var cell=this.shape.cells[i];
-			if(cell.c==0) return false;
-			else if(this.wall[cell.r][cell.c-1]) return false;
-		}return true;
+	paintScore(){
+		var spans=this.pg.getElementsByTagName("span");
+		spans[0].innerHTML=this.score;
+		spans[1].innerHTML=this.lines;
 	},
-	moveLeft(){
-		if(this.canLeft())
-		this.shape.moveLeft();
-	},
-	canRight(){
-		for(var i=0;i<this.shape.cells.length;i++){
-			var cell=this.shape.cells[i];
-			if(cell.c==this.CN-1) return false;//有错
-			else if(this.wall[cell.r][cell.c+1]) return false;
-		}
-		return true;
-	},
-	moveRight(){
-		if(this.canRight())
-		this.shape.moveRight();
-	},
-	isFullRow(r){
-		var reg=/^,|,,|,&/;
-		return !reg.test(String(this.wall[r]));
+	isFullRow(r){//判断r行是否满格
+		var reg=/^,|,,|,$/;
+		//用reg检查wall中r行转为字符串的结果
+		return !reg.test(String(this.wall[r]))
 	},
 	deleteRows(){//删除所有满格行
 		//自底向上遍历wall中每一行
@@ -88,10 +224,10 @@ var game={
 				break;
 			//如果r行是满格行
 			else if(this.isFullRow(r)){
-				this.deleteRow(r);
+				this.deleteRow(r);//就删除r行
 				ln++;
 				if(ln==4) break;
-				r++;
+				r++;//r留在原地
 			}
 		}
 		return ln;
@@ -99,69 +235,17 @@ var game={
 	deleteRow(r){//删除一行
 		//从r开始，反向遍历wall中每一行
 		for(;r>=0;r--){
-			//用r-1行代替r行
-			this.wall[r]=this.wall[r-1];
+			this.wall[r]=this.wall[r-1];//用r-1行代替r行
 			//将r-1行置为CN个空元素的数组
 			this.wall[r-1]=new Array(this.CN);
 			//遍历r行中每个格
 			for(var c=0;c<this.CN;c++){
 				if(this.wall[r][c])//如果当前位置有格
-					this.wall[r][c].r++;
+					this.wall[r][c].r++;//将当前格的r+1
 			}
 			//如果r-2行是空行,就退出循环
 			if(this.wall[r-2].join("")=="") break;
-		}	
-	},
-	paintShape(){//绘制主角图形
-		var frag=document.createDocumentFragment();
-		//遍历shape的cells中每个cell
-		for(var i=0;i<this.shape.cells.length;i++){
-			//将当前cell临时保存在cell中
-			var cell=this.shape.cells[i];
-			var img=new Image();//新建一个img
-			img.style.left=cell.c*this.CSIZE+this.OFFSET+"px";
-			//设置img的left为cell的c*CSIZE+OFFSET
-			img.style.top=cell.r*this.CSIZE+this.OFFSET+"px";
-			//设置img的top为cell的r*CSIZE+OFFSET
-			img.src=cell.src;//设置img的src为cell的src
-			frag.appendChild(img);
 		}
-		this.pg.appendChild(frag);	
-	},
-	paint(){//重绘一切
-		var reg=/<img [^>]*>/g;
-		this.pg.innerHTML=this.pg.innerHTML.replace(reg,"");
-		this.paintShape();//绘制主角
-		this.paintWall();//绘制方块墙
-	},
-	landIntoWall(){//4. 将shape中每个cell落入墙中
-		for(var i=0;i<this.shape.cells.length;i++){
-			//遍历shape的cells中每个cell
-			var cell=this.shape.cells[i];//将当前cell临时保存
-			this.wall[cell.r][cell.c]=cell;//将当前cell保存到wall中相同位置
-		}
-	},
-	paintWall(){//6. 专门绘制方块墙
-		//创建文档片段
-		var frag=document.createDocumentFragment();
-		//遍历wall中每个元素
-		for(var r=this.RN-1;r>=0;r--){
-			if(this.wall[r].join("")!=""){//如果当前行不是空行
-				for(var c=0;c<this.CN;c++){
-					if(this.wall[r][c]){//如果wall中当前位置有格
-						var cell=this.wall[r][c];//将当前cell临时保存在cell中
-						var img=new Image();//新建一个img
-						//设置img的left为cell的c*CSIZE+OFFSET
-						img.style.left=cell.c*this.CSIZE+this.OFFSET+"px";
-						img.style.top=cell.r*this.CSIZE+this.OFFSET+"px";
-						img.src=cell.src;//设置img的src为cell的src
-						frag.appendChild(img);//将img追加到frag中
-					}
-				}
-			}else break;//否则说明上方不再有格，就退出
-		}
-		this.pg.appendChild(frag);
-	},
-	
+	}
 }
-game.start();
+game.start();//启动游戏
